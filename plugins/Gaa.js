2b5e4a5a04cba566d97c9fb2ee59069548bd1981
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-03-30 13:52:25 -0500

    Update Gaa.js

--- plugins/Gaa.js
+++ plugins/Gaa.js
@@ -2,8 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Ga.mp3'
-conn.sendFile(m.chat, vn, 'Ga.mp3', null, m, true, {
+let vn = './media/Gaa.mp3'
+conn.sendFile(m.chat, vn, 'Gaa.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
