0658a3e5ddbf34f76904085d2eca13a8e2eb0ad2
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-03-30 14:02:06 -0500

    Update and rename gaa.js to Mega gaa.js

--- plugins/Mega gaa.js
+++ plugins/Mega gaa.js
@@ -8,6 +8,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /mega gaa|Mega Gaa/
+handler.customPrefix = /mega gaa|Mega gaa/
 handler.command = new RegExp
 module.exports = handler
