0cf06b2ed05eb998af363886d0e121845e6d2ecc
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-03-30 14:03:46 -0500

    Update and rename Mega gaa.js to Megaa.js

--- plugins/Megaa.js
+++ plugins/Megaa.js
@@ -2,12 +2,12 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Mega gaa.mp3'
-conn.sendFile(m.chat, vn, 'Mega gaa.mp3', null, m, true, {
+let vn = './media/Megaa.mp3'
+conn.sendFile(m.chat, vn, 'Megaa.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /mega gaa|Mega gaa/
+handler.customPrefix = /megaa|Megaa/
 handler.command = new RegExp
 module.exports = handler
