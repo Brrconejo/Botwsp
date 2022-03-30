7178dbca8ead2f12d66fdf40b6cd44726ef34832
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-03-30 13:56:06 -0500

    Update and rename Gaa.js to gaa.js

--- plugins/gaa.js
+++ plugins/gaa.js
@@ -2,8 +2,8 @@ let util = require('util')
 let path = require('path')
 
 let handler = async (m, { conn }) => {
-let vn = './media/Gaa.mp3'
-conn.sendFile(m.chat, vn, 'Gaa.mp3', null, m, true, {
+let vn = './media/gaa.mp3'
+conn.sendFile(m.chat, vn, 'gaa.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
