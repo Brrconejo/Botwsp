a6439b0696b6a92363c7a9369d7ab2086987981d
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-04-23 00:28:22 -0500

    Update menu de comandos.js

--- plugins/menu de comandos.js
+++ plugins/menu de comandos.js
@@ -65,6 +65,11 @@ let menu =`
 ┣ ඬ⃟🎖️ _${usedPrefix}topput@s_
 ┣ ඬ⃟🎖️ _${usedPrefix}toppajer@s_
 ┣ ඬ⃟🎖️ _${usedPrefix}topotakus_
+┣ ඬ⃟🎖️ _${usedPrefix}toput@s_
+┣ ඬ⃟🎖️ _${usedPrefix}toptop10integrantes_
+┣ ඬ⃟🎖️ _${usedPrefix}top10grasa_
+┣ ඬ⃟🎖️ _${usedPrefix}top10pana_
+┣ ඬ⃟🎖️ _${usedPrefix}top10shiposter_
 ┗━━━━━━━━━━━━━┛
 ┏━━━━━━━━━━━━━┓
 ┃ *<NUMERO DEL CREADOR/>*
