feb36d342f418e57a988f22dcef62f0d464287e6
Brrconejo <87559810+Brrconejo@users.noreply.github.com>
2022-04-24 08:02:09 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,6 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '51958959882', 'Shadow - Creador - Oficial 1', m)
-handler.command = /^(owner|dueño)$/i
+this.sendContact(m.chat, '5219992095479', 'Shadow - Creador - Oficial 1', m)
+this.sendContact(m.chat, '17722386341', 'Shadow - Creador - Oficial 2', m)}
+handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
